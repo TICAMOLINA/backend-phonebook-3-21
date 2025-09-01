e67d3519bd1909f4619ca7ac47cf4bf6b6b43519
Mauricio <ticam@Mauricio.com>
2025-09-01 09:48:17 -0300

    Ejercicio 3.14: Guarda persona nueva en base de datos. El post toma el req.body, almacenandolo en la constante newPerson. Se verifica que los campos name y phone de newPerson no sean undefined. Se utiliza el modelo Person para crear un nuevo objeto que almacena los campos nombres y numero de telefono en caso de que no esten vacios. el metodo save gestionado con then guarda el nuevo elemento en la base de datos. La obtencion de un elemento por get se realiza a traves del metodo findById aplicado al modelo Person, gestionado con then

--- models/person.js
+++ models/person.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 
 mongoose.set('strictQuery', false)
+//! El ajuste mongoose.set('strictQuery', false) controla cómo Mongoose maneja las propiedades que no existen en el esquema cuando hacés una consulta. Permite usar cualquier campo, aunque no esté en el schema.
 
 const url = process.env.MONGODB_URI
 //console.log('Coneccting to ', url)
@@ -20,12 +21,18 @@ const personSchema = new mongoose.Schema({
     phone: String,
 })
 
-personSchema.set('toJSON', {
+personSchema.set('toJSON', { // opción del schema que define cómo se serializa a JSON un documento Mongoose
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
+/*  Crea un campo id “lindo” para el front (string simple).
+_id es un ObjectId (tipo especial de MongoDB); pasarlo a string facilita usarlo como key en React, etc.*/
     delete returnedObject._id
+/* Oculta el campo _id original (ya no hace falta porque tenemos id).*/
     delete returnedObject.__v
+/* Quita __v, que es la clave de versión interna de Mongoose (sirve para control de concurrencia y versionado de documentos).
+No aporta nada al cliente y confunde.  */
   }
 })
 
 export default mongoose.model('Person', personSchema)
+// export default mongoose.model('Person', personSchema) crea y exporta por defecto un modelo de Mongoose llamado Person, basado en el personSchema. Este modelo representa la colección people en la base de datos y se usa para interactuar con ella (consultar, guardar, actualizar, eliminar documentos).
--- server.js
+++ server.js
@@ -39,18 +39,10 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    //! App llama al método GET definiendo en la ruta el id (:id), para obtener uno de los elementos
-    const id = Number(req.params.id)
-    //! define constante id la cual con el metodo number transforma a numero el id de la peticion, que se obtiene con el metodo params y id. Se transforma a numero porque este es del tipo string, y los ids que estan en los datos son numeros, por lo que en la posterior comparacion y busqueda si no se realiza la transformacion devolveria undefined
-    const person = persons.find((p) => p.id === id)
-    //! la constante person es una busqueda en el array persons (o sea los datos) que devolvera el elemento que resulte verdadero en la comparativa de los ids existentes con el obtenido en el req
-    if (person) {
-        res.json(person)
-        //! Si la busqueda anterior devuelve resultado, en la respuesta con metodo el metodo json se incluye dicho resultado 
-    } else {
-        res.status(404).end()
-        //! Si la busqueda no arroja resultado, se finaliza con un estado 404, sin devolverle dato alguno al usuario mas que el mensaje del error 404
-    }
+    const id = req.params.id
+    Person.findById(id).then(returnedPerson => {
+        res.status(200).json(returnedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -65,46 +57,23 @@ app.delete('/api/persons/:id', (req, res) => {
     //! La respuesta finaliza con un status 204, es decir una solicitud que fue procesada con exito pero que no necesita devolver dato alguno
 })
 
-const generateId = () => {
-    //? Funcion que crearetorna el resultado de la constante newId. Math random, devuelve un numero aleatorio entre 0 y 10000 (no incluidos), y math floor lo convierte al numero entero menor mas proximo
-    const newId = Math.floor(Math.random() * 10000)
-    return newId
-}
-
 app.post('/api/persons', (req, res) => {
     const newPerson = req.body
-    //! La constante newPerson obtiene el body de la peticion (las propiedades de cada elemento)
-    //? TENER EN CUENTA EL USO DEL MIDDLEWARE APP.USE(EXPRESS.JSON())
 
-    if (newPerson.name === "" || newPerson.phone === "") {
-        //! si en el body, los campos name y number vienen vacios se responde y finaliza la ejecucion del codigo con un status 400, y un mensaje de error con metodo json
+    if (newPerson.name === undefined || newPerson.phone === undefined) {
         return res.status(400).json({
             error: 'content missing'
         })
     }
 
-    const names = persons.map((p) => p.name)
-    //! Para propiciar una futura comparacion y verificar si el nombre existe, primero crea constante names (con map crea un nuevo array con los name de cada persona)
-    const matchName = names.includes(newPerson.name)
-    //! la constante matchName devuelve Boolean, con metodo include, verifica si entre los nombres del array creado con anterioridad se encuentra el nombre que viene en la peticion del usuario. 
-    if (matchName) {
-        //! si matchName es true, es decir si el nombre ya se encuentra en la agenda detiene la ejecucion del codigo con el estado de error 400, y una respuesta en json con el mensaje de que el nombre debe ser unico
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const addedPerson = {
-        //! En caso de superar todas las verificaciones se crea un nuevo objeto con los datos name y number de la peticion, y se incluye un id que es el numero obtenido de la funcion generateId, la cual se ejecuta en este punto
+    const person = new Person({
         name: newPerson.name,
-        phone: newPerson.phone,
-        id: generateId()
-    }
+        phone: newPerson.phone
+    })
 
-    persons = persons.concat(addedPerson)
-    //! se reemplaza los datos originales con el metodo concat, el cual agregara en un nuevo array los datos del objeto anterior
-    res.status(201).json(addedPerson)
-    //! se devuelve una respuesta con codigo 201 (de creacion exitosa), y mostrara con json los datos nuevos
+    person.save().then(savedPerson => {
+        res.status(201).json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT
